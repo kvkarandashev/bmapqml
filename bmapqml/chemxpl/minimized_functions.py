6267a63fe7b916b382c935e9ded995d9b945b289
janweinreich <janweinreich286@gmail.com>
2022-06-03 11:06:28 +0200

    simple class to combine minimized functions

--- bmapqml/chemxpl/minimized_functions.py
+++ bmapqml/chemxpl/minimized_functions.py
@@ -36,7 +36,62 @@ class QM9_properties:
     
         _, _, _, canon_SMILES = chemgraph_to_canonical_rdkit(
             trajectory_point_in.egc.chemgraph)
+        print(canon_SMILES)
         X_test = dc.feat.RDKitDescriptors(
             ipc_avg=True).featurize([canon_SMILES])
         prediction = self.scaler_function.inverse_transform(self.ml_model.predict(X_test).reshape(-1,1))
         return prediction[-1]
+
+class multi_obj:
+
+    """
+    Combine multiple minimize functions in various different ways.
+    Adjust weights for each property necessary because properties live on different orders 
+    of magnitude. Clever way might be to normalize each property by the initial
+    value at the fist point of the trajectory
+
+    fct_list    : List of minimized functions
+    fct_weights : Weights between minimized functions, len(fct_weights) == len(fct_list)
+    """
+
+    def __init__(self, fct_list, fct_weights, init_gc, normalize=True):
+
+        self.fct_list   = fct_list
+        self.fct_weights = fct_weights
+        self.normalize = normalize
+        self.init_gc    = init_gc
+
+
+        """
+        trajectory_point_in must be initial cg!!!
+        but getting an error with how i implemented it below
+        if self.normalize:
+            self.fct_initial = []
+            for fct in zip(self.fct_list):
+                #print(self.fct_list)
+                #print(fct)
+                print(self.init_gc)
+                print(fct[0].__call__(self.init_gc[0]))
+                self.fct_initial.append(fct[0].__call__(self.init_gc))
+                #print(fct[0].__call__(self.init_gc))
+        """
+
+    def __call__(self,trajectory_point_in):
+        
+
+        s = 0
+        for fct, w in zip(self.fct_list,self.fct_weights): 
+            
+            """
+            uncomment as soon as there is a way to get the initial values
+            of each property. 
+            Possible evaluate the properties in parallel to make it faster
+            """
+
+            #, self.fct_initial):
+            #value = (w/abs(fct_init)) * fct.__call__(trajectory_point_in)
+
+            value = w * fct.__call__(trajectory_point_in)
+            s+=value
+
+        return s
