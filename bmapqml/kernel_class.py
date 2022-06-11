5c71bed692493bc82ce198592cb21a5e89d8d635
janweinreich <janweinreich286@gmail.com>
2022-06-11 13:44:06 +0200

    kernel class nows allows feature scaling

--- bmapqml/kernel_class.py
+++ bmapqml/kernel_class.py
@@ -2,31 +2,86 @@
 Simple sklearn style class for kernel ridge regression (KRR)
 using the BIGMAPQML library.
 """
+import pdb
 
 class KRR():
     
-    def __init__(self, kernel_type):
+    def __init__(self, kernel_type, scale_features=True, scale_labels=True):
         
         """
         User must provide a kernelfunction
         kernel_type: string
         """
-        self.kernel_type = kernel_type
+        self.kernel_type    = kernel_type
+        self.scale_features = scale_features
+        self.scale_labels   = scale_labels
 
+    #X_std = (X - X.min(axis=0)) / (X.max(axis=0) - X.min(axis=0))
+    #X_scaled = X_std * (max - min) + min
 
+    def X_MinMaxScaler(self, X):   
+
+        """
+        MinMaxScaler for the features.
+        X: numpy array
+        """
+        import numpy as np
+
+        a = 0
+        b = 1
+        X_scaled = a + ((X - self.X_minval) * (b - a) / (self.X_maxval - self.X_minval))
+        return X_scaled
+
+    def y_MinMaxScaler(self, y):
+            
+        """
+        MinMaxScaler for the labels.
+        y: numpy array
+        """
+        import numpy as np
+        y = y.reshape(-1, 1)
+        a = 0
+        b = 1
+        y_scaled = a + ((y - self.y_minval) * (b - a) / (self.y_maxval - self.y_minval))
+        return y_scaled.flatten()
+
+    def y_MinMaxScaler_inverse(self, y):
+            
+        """
+        Inverse MinMaxScaler for the labels.
+        y: numpy array
+        """
+        import numpy as np
+        y = y.reshape(-1, 1)
+        a = 0
+        b = 1
+        y_inverse = (y - a) * ((self.y_maxval - self.y_minval) / (b - a)) + self.y_minval
+        return y_inverse.flatten()
 
     def fit(self, X_train, y_train):
-        
+
         """
         Fit kernel model to the training data and perform a hyperparameter search.
         X_train: numpy array of representations of the training data
         y_train: numpy array of labels of the training data
         """
-
         import numpy as np
         from bmapqml.kernels import laplacian_sym_kernel_matrix, gaussian_sym_kernel_matrix
         from bmapqml.hyperparameter_optimization import stochastic_gradient_descend_hyperparam_optimization    
         from bmapqml.linear_algebra import scipy_cho_solve
+
+        if self.scale_features:
+            self.X_maxval  = np.max(X_train)
+            self.X_minval  = np.min(X_train)
+            X_train = self.X_MinMaxScaler(X_train)
+
+        if self.scale_features:
+            self.y_maxval  = np.max(y_train)
+            self.y_minval  = np.min(y_train)
+            y_train = self.y_MinMaxScaler(y_train)
+
+
+        #pdb.set_trace()
 
         if self.kernel_type == "gaussian":
             self.kernel_func = gaussian_sym_kernel_matrix
@@ -72,13 +127,21 @@ class KRR():
             raise ValueError("Kernel type not supported")
 
 
+        if self.scale_features:
+            X_test = self.X_MinMaxScaler(X_test)
+
+
+
         K_test  =   self.kernel_func(X_test, self.X_train, self.sigmas)
         y_pred  =   np.dot(K_test, self.alphas)
 
+        if self.scale_features:
+            y_pred = self.y_MinMaxScaler_inverse(y_pred)
 
         return y_pred
 
     def save(self, filename):
+
         from bmapqml.utils import dump2pkl
 
         """
