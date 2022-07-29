59b801e110f7cae1eb45976783ad486cb4aa2581
kvkarandashev <kvkarandashev@gmail.com>
2022-07-29 19:43:30 +0200

    Test scripts.

---
+++ examples/chemxpl/MC_minimization/long_run_scripts/submit_all_bias_test_run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Replace with wherever you want to store the results.
+dump_directory="/store/common/konst/chemxpl_related/empty_minimization_runs"
+
+min_func_directory="/store/common/konst/chemxpl_related"
+
+min_func_names=("electrolyte")
+
+job_name=$1
+
+if [ "$job_name" == "" ]
+then
+    echo "Job name should be second argument"
+    exit
+fi
+
+cur_directory=$(pwd)
+
+py_script=$cur_directory/MC_bias.py
+
+
+for min_func_name in ${min_func_names[@]}
+do
+    final_dump_directory=$dump_directory/${job_name}_$min_func_name
+
+    mkdir -p $final_dump_directory
+    cd $final_dump_directory
+    for seed in $(seq 1 8)
+    do
+        spython --OMP_NUM_THREADS=1 --CPUs=1 $py_script ${job_name}_$seed TRIAL_RUN $min_func_name $seed
+    done
+done
