e87f30fa8e64ac7c997afe870d10977033fe11e2
kvkarandashev <kvkarandashev@gmail.com>
2022-08-01 14:52:58 +0200

    Better plotting scripts.

--- examples/chemxpl/plotting/plot_all_folder_min_vals_2.py
+++ examples/chemxpl/plotting/plot_all_folder_min_vals_2.py
@@ -1,19 +1,33 @@
-import sys, os, glob, math
+import sys, glob, math
 import matplotlib.pyplot as plt
+import numpy as np
+
+plt.figure(figsize=(8.0, 6.0))
 
 colors = ["blue", "red", "green", "yellow"]
 
 quant_id = {"min_func_val": 1, "hist_size": 2}
 
+plt.rcParams.update({"font.size": 16})
+
+axis_label_fontsize = 20
+legend_fontsize = 20
+
+last_linearity = 90000
+
+
 for quant in ["min_func_val", "hist_size"]:
-    for folder_id, folder in enumerate(sys.argv[1:]):
+    for folder_id, folder_pair in enumerate(sys.argv[1:]):
+        folder_pair_split = folder_pair.split(":")
+        folder = folder_pair_split[1]
         cur_color = colors[folder_id]
         files = glob.glob(folder + "/*.expl_data")
         files.sort()
-        legend = folder
+        legend = folder_pair_split[0].replace("_", " ")
+        cur_legend = legend
         for f_id, f in enumerate(files):
             if f_id == 1:
-                legend = None
+                cur_legend = None
             inp_f = open(f, "r")
             lines = inp_f.readlines()
             inp_f.close()
@@ -26,7 +40,22 @@ for quant in ["min_func_val", "hist_size"]:
                 if quant == "hist_size":
                     y_val = math.log(y_val)
                 y_vals.append(y_val)
-            plt.plot(x_vals, y_vals, label=legend, color=cur_color, linestyle="dashed")
-    plt.legend()
+            plt.plot(
+                x_vals, y_vals, label=cur_legend, color=cur_color, linestyle="dashed"
+            )
+        if quant == "hist_size":
+            x_lin = np.array(x_vals[-last_linearity:])
+            y_lin = np.array(y_vals[-last_linearity:])
+            linear_model = np.polyfit(x_lin, y_lin, 1)
+            print("Linear fit coefficients for ", legend, ":", linear_model)
+
+    plt.legend(fontsize=legend_fontsize, frameon=False)
+    plt.xlabel(r"$\mathrm{ln}(N_{\mathrm{MC\,\,steps}})$", fontsize=axis_label_fontsize)
+    if quant == "hist_size":
+        ylabel = r"$\mathrm{ln}(N_{\mathrm{histogram}})$"
+    else:
+        ylabel = r"best $F$ value"
+
+    plt.ylabel(ylabel, fontsize=axis_label_fontsize)
     plt.savefig(quant + ".png")
     plt.clf()
