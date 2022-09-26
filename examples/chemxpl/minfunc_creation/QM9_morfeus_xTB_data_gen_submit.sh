3e7807fb358171120ce5cb8ce24d0ee8830f6df8
kvkarandashev <kvkarandashev@gmail.com>
2022-09-26 14:22:11 +0200

    Clearer comments.

--- examples/chemxpl/minfunc_creation/QM9_morfeus_xTB_data_gen_submit.sh
+++ examples/chemxpl/minfunc_creation/QM9_morfeus_xTB_data_gen_submit.sh
@@ -5,5 +5,5 @@ CPUS=2
 
 for solvent in water ether acetonitrile dmso thf
 do
-    spython --OMP_NUM_THREADS=2 --CPU=$CPUS QM9_morfeus_xTB_data_gen.py qm9_morfeus_xTB_solv_$solvent $solvent $((NPROCS/CPUS))
+    spython --OMP_NUM_THREADS=$CPUS --CPUs=$NPROCS QM9_morfeus_xTB_data_gen.py qm9_morfeus_xTB_solv_$solvent $solvent $((NPROCS/CPUS))
 done
