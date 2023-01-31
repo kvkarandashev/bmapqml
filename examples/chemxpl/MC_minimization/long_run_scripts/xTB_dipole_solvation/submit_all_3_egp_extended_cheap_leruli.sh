245b0006e6d7810bb89bdbf7eb35593a6884919f
kvkarandashev <kvkarandashev@gmail.com>
2023-01-31 16:39:46 +0100

    Script for leruli submissions.

---
+++ examples/chemxpl/MC_minimization/long_run_scripts/xTB_dipole_solvation/submit_all_3_egp_extended_cheap_leruli.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# The minimized function file is created by scripts in examples/chemxpl/minfunc_creation/morfeus_xTB_solvation.
+dump_directory="/store/konst/chemxpl_related/minimization_runs_xTB_dipole_solvation_cheap_leruli"
+
+gap_constraints=("weak" "strong")
+
+quantities=("solvation_energy" "dipole") # "atomization_energy")
+
+bias_strengths=("stronger" "weak" "none")
+
+job_name=$1
+
+if [ "$job_name" == "" ]
+then
+    echo "Job name should be second argument"
+    exit
+fi
+
+cur_directory=$(pwd)
+
+py_script=$cur_directory/MC_opt_3_egp.py
+
+NCPUs=8
+
+common_flags="--memory=$((NCPUs*10000)) --CPUs=$NCPUs --OMP_NUM_THREADS=1 --export_env_vars=MORFEUS_NUM_THREADS=$NCPUs/OMP_NUM_THREADS=$((NCPUs/2)),1/OMP_MAX_ACTIVE_LEVELS=1 --update_bmapqml"
+
+for bias_strength in ${bias_strengths[@]}
+do
+    for gap_constraint in ${gap_constraints[@]}
+    do
+        for quantity in ${quantities[@]}
+        do
+            cur_job_name=${job_name}_${bias_strength}_${gap_constraint}_${quantity}
+            min_func_file=$cur_directory/egp_morfeus_xTB_cheap_water_${quantity}_${gap_constraint}.pkl
+            final_dump_directory=$dump_directory/${cur_job_name}
+
+            mkdir -p $final_dump_directory
+            cd $final_dump_directory
+            for seed in $(seq 1 8)
+            do
+                req_files=$min_func_file
+                seed_name=${cur_job_name}_$seed
+                data_subdir=${cur_job_name}/data_$seed_name
+                if [ -d $dump_directory/completed/$data_subdir ]
+                then
+                    continue
+                fi
+                other_file=$dump_directory/latest_batch/$data_subdir/restart_file_$seed.pkl
+                if [ -f $other_file ]
+                then
+                    req_files=$req_files:$other_file
+                fi
+                lpython $common_flags --req_files=$req_files $py_script $seed_name $(basename $min_func_file) min_$quantity $seed $bias_strength
+            done
+            cd $cur_directory
+        done
+    done
+done
