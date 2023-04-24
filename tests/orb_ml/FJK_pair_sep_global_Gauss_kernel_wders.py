baeecb3c5a1fae9070cce5f633172ba10f809514
kvkarandashev <kvkarandashev@gmail.com>
2023-04-24 19:16:35 +0200

    Merged with qml-2.0 version of precompiled. Added a dedicated script checking NumBa vs Fortran timing.

--- tests/orb_ml/FJK_pair_sep_global_Gauss_kernel_wders.py
+++ tests/orb_ml/FJK_pair_sep_global_Gauss_kernel_wders.py
@@ -6,28 +6,33 @@ import numpy as np
 import random, sys
 
 # Toggle to use Fortran or Numba implementation.
-use_Fortran=True
+if len(sys.argv) < 3:
+    use_Fortran = True
+else:
+    use_Fortran = sys.argv[2] == "True"
 
-seed=1
-num_test_mols_1=50
-num_test_mols_2=40
+seed = 1
+num_test_mols_1 = 50
+num_test_mols_2 = 40
 
-logfile_name=sys.argv[1]
+logfile_name = sys.argv[1]
 
-test_xyz_dir="./qm7"
-all_xyzs=dirs_xyz_list(test_xyz_dir)
+test_xyz_dir = "./qm7"
+all_xyzs = dirs_xyz_list(test_xyz_dir)
 
-tested_xyzs_1=random.Random(seed).sample(all_xyzs, num_test_mols_1)
-tested_xyzs_2=random.Random(seed+1).sample(all_xyzs, num_test_mols_2)
+tested_xyzs_1 = random.Random(seed).sample(all_xyzs, num_test_mols_1)
+tested_xyzs_2 = random.Random(seed + 1).sample(all_xyzs, num_test_mols_2)
 
-logfile=logfile(logfile_name)
+logfile = logfile(logfile_name)
 
-Slater_pair_kwargs={"second_oml_comp_kwargs" : {"used_orb_type" : "HOMO_removed", "calc_type" : "UHF"}}
+Slater_pair_kwargs = {
+    "second_oml_comp_kwargs": {"used_orb_type": "HOMO_removed", "calc_type": "UHF"}
+}
 
-oml_compounds_1=OML_Slater_pair_list_from_xyzs(tested_xyzs_1, **Slater_pair_kwargs)
-oml_compounds_2=OML_Slater_pair_list_from_xyzs(tested_xyzs_2, **Slater_pair_kwargs)
+oml_compounds_1 = OML_Slater_pair_list_from_xyzs(tested_xyzs_1, **Slater_pair_kwargs)
+oml_compounds_2 = OML_Slater_pair_list_from_xyzs(tested_xyzs_2, **Slater_pair_kwargs)
 
-orb_rep_params=OML_rep_params(max_angular_momentum=1, orb_atom_rho_comp=0.95)
+orb_rep_params = OML_rep_params(max_angular_momentum=1, orb_atom_rho_comp=0.95)
 
 timestamp("Representation generation.")
 oml_compounds_1.generate_orb_reps(orb_rep_params, fixed_num_threads=1)
@@ -35,13 +40,13 @@ oml_compounds_2.generate_orb_reps(orb_rep_params)
 timestamp("Finished.")
 
 timestamp("Standard deviation calculations.")
-av_vals, width_params=oml_ensemble_avs_stddevs(oml_compounds_1)
+av_vals, width_params = oml_ensemble_avs_stddevs(oml_compounds_1)
 timestamp("Finished.")
 
 logfile.write("Width params")
 logfile.write(width_params)
 
-sigmas=np.array([0.5, *width_params])
+sigmas = np.array([0.5, *width_params])
 
 logfile.write("xyz list 1")
 logfile.write(tested_xyzs_1)
@@ -56,15 +61,19 @@ else:
 
 logfile.write("kernel_11")
 timestamp("Symmetrical kernel calculation.")
-kernel_wders=gauss_sep_orb_sym_kernel(oml_compounds_1, sigmas, with_ders=True, global_Gauss=True)
+kernel_wders = gauss_sep_orb_sym_kernel(
+    oml_compounds_1, sigmas, with_ders=True, global_Gauss=True
+)
 timestamp("Finished.")
-#logfile.export_matrix(kernel_wders)
-logfile.randomized_export_3D_arr(kernel_wders, seed+2)
+# logfile.export_matrix(kernel_wders)
+logfile.randomized_export_3D_arr(kernel_wders, seed + 2)
 
 logfile.write("kernel_12")
 timestamp("Asymmetrical kernel calculation.")
-kernel_wders=gauss_sep_orb_kernel(oml_compounds_1, oml_compounds_2, sigmas, with_ders=True, global_Gauss=True)
+kernel_wders = gauss_sep_orb_kernel(
+    oml_compounds_1, oml_compounds_2, sigmas, with_ders=True, global_Gauss=True
+)
 timestamp("Finished.")
-#logfile.export_matrix(kernel_wders)
-logfile.randomized_export_3D_arr(kernel_wders, seed+2)
+# logfile.export_matrix(kernel_wders)
+logfile.randomized_export_3D_arr(kernel_wders, seed + 2)
 logfile.close()
